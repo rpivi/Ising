d89b189579b44c1484362a64782036183b084db4
rpivi <rickpiwi25@gmail.com>
2026-01-26 17:44:38 +0100

    the thermal equilibrium!

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ def main():
     L = 50  # Lattice size
     N = L * L  # Total number of spins
     BJ = 0.7  # Inverse temperature
-    steps = 100000 +1 # Number of Monte Carlo steps
+    nsweep = 500  # Number of sweeps
 
     spins_r = lat.create_lattice(L, initial_state='random')
     spins_u = lat.create_lattice(L, initial_state='up')
 
-    initial_energy_r = ph.total_energy(spins_r)
-    initial_energy_u = ph.total_energy(spins_u)
-
-    net_spins_r, net_energies_r = metro.metropolis(spins_r, steps, BJ, initial_energy_r)
-    net_spins_u, net_energies_u = metro.metropolis(spins_u, steps, BJ, initial_energy_u)
+    net_spins_r, net_energies_r = metro.metropolis(spins_r, nsweep, BJ)
+    net_spins_u, net_energies_u = metro.metropolis(spins_u, nsweep, BJ)
 
     plot.plot_two_steps(net_spins_r/N, net_spins_u/N, name1="Mean Spins (Random Init)", name2="Mean Spins (Up Init)", BJ=BJ)
     plot.plot_two_steps(net_energies_r, net_energies_u, name1="Net Energy (Random Init)", name2="Net Energy (Up Init)", BJ=BJ)
--- metropolis.py
+++ metropolis.py
@@ -1,31 +1,37 @@
 import numpy as np
 import physical_quant as ph
 
-def metropolis(spins, times, BJ, energy):
-    spins_arr = spins.copy()
-    net_spins = np.zeros(times-1)
-    net_energies = np.zeros(times-1)
-    L = spins_arr.shape[0]
+def metropolis(spins, nsweep, BJ):
 
-    for t in range(0,times-1):
+    #settings 
+    spins_arr = spins.copy()
+    energy = ph.total_energy(spins_arr)
+    L = spins_arr.shape[0]
+    steps = nsweep * L * L
+    net_spins = np.zeros(nsweep)
+    net_energies = np.zeros(nsweep)
+  
+    for t in range(0, steps):   
         i = np.random.randint(0, L)
         j = np.random.randint(0, L)
         spin_i = spins_arr[i, j]
         spin_f = -spin_i
 
         # Calculate the change in energy
-        spin_i = spins[i, j]
-        neighbors = spins[(i+1)%L, j] + spins[(i-1)%L, j] + spins[i, (j+1)%L] +spins[i, (j-1)%L] #periodic boundary conditions
+        neighbors = (spins_arr[(i+1)%L, j] +
+                     spins_arr[(i-1)%L, j] + 
+                     spins_arr[i, (j+1)%L] +
+                     spins_arr[i, (j-1)%L]) #periodic boundary conditions
+        
         dE = 2 * spin_i * neighbors
+
         # Metropolis criterion
         if (dE <= 0 ) or (np.random.rand() < np.exp(-BJ * dE)):
             spins_arr[i, j] = spin_f
             energy += dE
-        else:
-            spins_arr[i, j] = spin_i
-        if (t % 1000 == 0):
-            print(f"Step {t}/{times-1} completed.")
-            
-        net_spins[t] = spins_arr.sum()
-        net_energies[t] = energy
+        if (t % (L*L) == 0):
+            s = t//(L*L)
+            print(f"Sweep {s} / {nsweep} completed.")
+            net_spins[s] = abs(spins_arr.sum())
+            net_energies[s] = energy
     return net_spins, net_energies
